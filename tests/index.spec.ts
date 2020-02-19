5e80ff889c5f832f9dac4adc884b157722d9fca8
Vajahath Ahmed <vajuoff.1@gmail.com>
2020-02-19 22:55:17 +0530

    clearing tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,10 +3,8 @@
  * gives you the ability to test the exact
  * code getting published. You can also check the typings this way.
  */
-const { add } = require('../dist/index');
-
 describe('testing add function', () => {
-  test('5 + 6 = 11', () => {
-    expect(add(5, 6)).toBe(11);
+  test('comeback here later', () => {
+    expect(true).toBe(true);
   });
 });
